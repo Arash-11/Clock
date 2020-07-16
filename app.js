6b23911557629fbf78ae7fedb1ed3b65fa43858c
Arash-11 <62180768+Arash-11@users.noreply.github.com>
2020-07-15 22:26:03 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+let hoursHand = document.querySelector('.hour-hand'),
+    minutesHand = document.querySelector('.minute-hand'),
+    secondsHand = document.querySelector('.second-hand');
+
+
+function updateTime() {
+
+  // get time information. This has to be continously updated, hence why we're declaring it here and not on the global window.
+  let today = new Date();
+
+  let hour,
+      minute = today.getMinutes(),
+      second = today.getSeconds();
+
+
+  // convert 24-hour based time to 12-hour based time.
+  if (today.getHours() <= 12) hour = today.getHours();
+
+  else if (today.getHours() > 12 && today.getHours() <= 24 ) {
+    hour = today.getHours() - 12;
+  };
+
+
+  // make each hand on the clock move and reflect the correct time.
+  let hoursAngle = hour * 30;
+      hoursHand.style.transform = 'translate(-50%, -50%) rotate(' + hoursAngle + 'deg)';
+
+  let minutesAngle = minute * 6;
+      minutesHand.style.transform = 'translate(-50%, -50%) rotate(' + minutesAngle + 'deg)';
+
+  let secondsAngle = second * 6;
+      secondsHand.style.transform = 'translate(-50%, -50%) rotate(' + secondsAngle + 'deg)';
+
+
+}
+
+
+// this IIFE will create a loop and keep the time updated
+( function() {
+  setInterval(updateTime, 1000);
+}() );
